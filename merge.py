18715cae3c82abe1cfa9507a863d64ca3f1baabb
Veilv212 <santiagovaldiviezo@MacBook-Air-de-Thiago.local>
2025-08-22 13:14:39 -0500

    2do commit

--- merge.py
+++ merge.py
@@ -1,4 +1,6 @@
 
+# Merge recursivo:
+
 def quicksort(lista):
     if len(lista) <= 1:
         return lista
@@ -6,14 +8,16 @@ def quicksort(lista):
         pivote = lista[0]
         menores = []
         mayores = []
-        
+
         for i in range(1,len(lista)):
             if lista[i] <= pivote:
                 menores.append(lista[i])
             else:
                 mayores.append(lista[i])
+            
         return quicksort(menores) + [pivote] + quicksort(mayores)
-    
-mi_lista = [5, 2, 10, 83, 21]
+        
+mi_lista = [2,4,21,3,6,10,23]
 ordenada = quicksort(mi_lista)
 print(ordenada)
+
