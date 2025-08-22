74170273fb6b2086eb3c4f437fd82d7af24be744
Veilv212 <santiagovaldiviezo@MacBook-Air-de-Thiago.local>
2025-08-22 12:43:36 -0500

    1er commit

---
+++ prueba.py
@@ -0,0 +1,9 @@
+# Comparando cada lista, dime los numeros que se repiten y muestralos en una nueva lista.
+
+a = [1,2,3,5]
+b = [3,2,6,7]
+c = []
+
+for i in range(len(a)):
+    if a[i] == b[i]:
+        print(b)
